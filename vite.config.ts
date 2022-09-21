48b6944a337da17a5b3a615c79990b644f65e8a2
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-09-21 15:03:22 +0200

    feat: add basic pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,37 @@
 import preact from '@preact/preset-vite'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    VitePWA({
+      includeAssets: ['images/favicon.ico', 'images/apple-touch-icon.png', 'images/masked-icon.svg'],
+      manifest: {
+        description: '// TODO',
+        icons: [
+          {
+            sizes: '192x192',
+            src: 'images/pwa-192x192.png',
+            type: 'image/png',
+          },
+          {
+            sizes: '512x512',
+            src: 'images/pwa-512x512.png',
+            type: 'image/png',
+          },
+          {
+            purpose: 'any maskable',
+            sizes: '512x512',
+            src: 'images/pwa-512x512.png',
+            type: 'image/png',
+          },
+        ],
+        name: '// TODO',
+        short_name: '// TODO',
+        theme_color: '#ffffff',
+      },
+      registerType: 'autoUpdate',
+    }),
+  ],
 })
